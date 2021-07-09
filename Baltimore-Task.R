8f4bb01dbf095989c5965414763dae2c033b38d2
Elizabeth-29 <66197915+Elizabeth-29@users.noreply.github.com>
2021-07-09 14:29:39 +0100

    Add files via upload

--- Baltimore-Task.R
+++ Baltimore-Task.R
@@ -1,36 +1,59 @@
+#Install all the necessary pakages
+
+install.packages('tidyverse')
+library('dplyr')
+library(tidyverse)
 
 # Firstly we import and view the dataset
 Baltimore <- read.csv('C:/Users/Mayowa/Documents/INTERNSHP/Baltimore_detections.csv')
 view(Baltimore)
 
 #Now we do the first cleanup exercise by removing unwanted columns
-Baltimore <- Baltimore[, -c(4,5,6,7)]
+Baltimore <- Baltimore[, -c(3,4,5,6,7)]
 
 #The first task is to create an ID column in other to uniquely identify duplicate image files
 Baltimore <- cbind(ID = 1:nrow(Baltimore), Baltimore)
 
 #The second task is to separate the variables in the labels column to multiple columns(Labels & Probability)
-library(tidyverse)
+
 Baltimore <- separate(Baltimore, label, into = c("Labels","Probability"),sep =":")
 
 #Now we remove the 'Cargo' and 'Rickshaw' data from the labels column
-Baltimore = filter(Baltimore, Labels!="rickshaw")
+Baltimore_new = filter(Baltimore, Labels!="rickshaw")
+Baltimore_new = filter(Baltimore_new, Labels!="cargo")
 
 #Lets replace our blank cells in the labels column with NA
 
-Baltimore$Labels[Baltimore$Labels==""] <- NA
+Baltimore_new$Labels[Baltimore_new$Labels==""] <- NA
+
+#To replace NA with Unknown ( I run this but it doesnt reflect in my dataset but it shows up in my console)
+Baltimore_new$Labels %>% replace_na(list(Labels = "unknown"))
+
 
 #The next is to view the proportion of unknown images to the known ones
 
 library(mice)
-md.pattern(Baltimore)
+md.pattern(Baltimore_new)
 
-#Convert the values from NA in the Labels column to Unknown
+#Creating new columns to hold pedals and motor values ( This one too did not show up in my dataset but i saw it in the console)
+Baltimore_new %>%
+  mutate(R1 = case_when(
+    endsWith(Labels, "r") ~ "motor",
+    endsWith(Labels, "A") ~ "Unknown"
+  ))
+
 
 #Calculating the ratio of number of images with pedals with 250 images
+#Firstly we randomly extract 250 entries giving the subset a new name
+#Then we get the ratio 
+
+Baltimore2 <- sample_n(Baltimore_new, 250, replace = FALSE)
+round(prop.table(table(Baltimore2$Labels, useNA = "ifany")),4) 
+
 
 #Absolute frequency without missing values
 freq<-table(Baltimore$Labels)
+
 freq
 
 #Absolute frequency with missing values
@@ -51,15 +74,13 @@ Baltimore_b<- Baltimore[dummy_sep == 1,]
 
 
 
+round(prop.table(table(Baltimore_new$Labels, useNA = "ifany")),4) 
 
+Baltimore2 <- sample_n(Baltimore_new, 250, replace = FALSE)
 
+Baltimore_new2 <- anti_join(Baltimore_new, Baltimore2)# this is done to create another random set of data without including the one I had used previously
 
-
-
-
-
-
-
+Baltimore3 <- sample_n(Baltimore_new2, 500, replace = FALSE)
 
 
 
